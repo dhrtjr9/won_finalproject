77eeba5ae262fecc3b69858842b601741dcd1050
Lee Ok Seok <dhrtjr9@gmail.com>
2023-08-21 19:38:22 +0900

    0821_add_main

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import { useState } from 'react';
 import './App.css';
-import { Navbar, Container, Nav, Card, Button} from 'react-bootstrap';
-import 작명 from './data.js';
+import { Navbar, Container, Nav, Card, Button, NavLink} from 'react-bootstrap';
+import { BrowserRouter } from 'react-router-dom';
+// import 작명 from './data.js';
+import { Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
+import imgFile from "./WON-logo-white.png"
+import Credit from './pages/Credit';
 
 function App() {
-  
+
   // Data.js 파일에서 가져 오기 위한 함수
   let [card] = useState()
-  
+  let navigate = useNavigate(); //훅
+
   return (
-    <div className='header_wrap'>
     <div className="App">
+
+    <div className='header_wrap'>
       <div className='nav_logo'>
       <Navbar bg="primary" data-bs-theme="dark" className='won-nav'>
         <Container>
-          <Navbar.Brand href="#home">WON</Navbar.Brand>
+          <Navbar.Brand href="#home" className='won-font'>WON</Navbar.Brand> 
           <Nav className="me-auto">
-            <Nav.Link href="#credit">신용카드</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/') }}>HOME</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/credit') }}>신용카드</Nav.Link>
+            {/* <Nav.Link href="#credit">신용카드</Nav.Link> */}
             <Nav.Link href="#credit">대출</Nav.Link>
             <Nav.Link href="#credit">보험</Nav.Link>
             <Nav.Link href="#credit">예적금</Nav.Link>
@@ -29,96 +37,112 @@ function App() {
           </form>
         </Container>
       </Navbar>
-      <br />
-      
-      <div className='main-bg'></div>
 
-      <br />
-     
-    <div className='card-parent'>
-      <Card style={{ width: '36rem' }}>
-        <Card.Img variant="top" src="./images/logo512.png/100px180" />
-        <Card.Body>
-          <Card.Text>
-          비회원도 이용할 수 있는 신용카드 추천 서비스
-          나에게 적합한 카드는 무엇일까요?
-          </Card.Text>
-          <Button variant="primary">기본 추천 받기</Button>
-        </Card.Body>
-      </Card>
-      <li></li>
-      <Card style={{ width: '36rem' }}>
+
+
+      <Routes>
+        <Route path='/' element={
+        <>
+        <br />
+      
+        <div className='main-bg'></div>
+  
+        <br />
+       
+      <div className='card-parent'>
+        <Card style={{ width: '36rem' }}>
+        <Card.Img variant="top" src="./images/recom_image2.jpg" height="270"/>
+          <Card.Body>
+            <Card.Text>
+            비회원도 이용할 수 있는 신용카드 추천 서비스 <br></br>
+            나에게 적합한 카드는 무엇일까요?
+            </Card.Text>
+            {/* <Button variant="primary">기본 추천 받기</Button> */}
+            <Button href="/credit">기본 추천 받기</Button>
+            
+          </Card.Body>
+        </Card>
+        <li></li>
+        <Card style={{ width: '36rem' }}>
+          <Card.Img variant="top" src="./images/recom_image1.jpg" height="270"/>
+          <Card.Body>
+            <Card.Text>
+            나와 적합한 신용카드와 카드 분석 리포트까지 <br></br>
+            지금 바로 이용해보세요
+            </Card.Text>
+            <Button href="/credit">회원 추천 받기</Button>
+          </Card.Body>
+        </Card>
+        
+        {/* <div className='card-main'></div>
+        <li></li>
+        <div className='card-main1'></div> */}
+      </div>
+  
+      <br></br>
+  
+      <div className='card-product'>
+      <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
+          <Card.Title>Card Title</Card.Title>
           <Card.Text>
-          나와 적합한 신용카드와 카드 분석 리포트까지
-          지금 바로 이용해보세요
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
           </Card.Text>
-          <Button variant="primary">회원 추천 받기</Button>
+          <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
-      
-      {/* <div className='card-main'></div>
       <li></li>
-      <div className='card-main1'></div> */}
-    </div>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      <li></li>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      <li></li>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      </div>
+      </>
+        } />
+        <Route path='/credit' element={<Credit/>}/>
+        {/* // 카드 상품 내 상세페이지 */}
+        <Route path='/credit_modal' element={<credit/>} />
+      </Routes>
 
-    <br></br>
-
-    <div className='card-product'>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    <li></li>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    <li></li>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    <li></li>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    </div>
+      
 
           </div>
         </div>  
       </div>
-  //   </div>
+  //</div>
   // </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,6 +1,13 @@
-let a = 10;
+// let card_info[{
+//     card_corp :
+//     card_name :
+//     benefit_main:
+//     overseas_yn
+//     overseas_fee
+    
+// }]
 
 // export 하려면 export default 변수명 입력
-export default a;
+// export default a;
 
 // 다른 파일에 있던 변수 가져오려면, 변수를 export하고 import하면 끝
